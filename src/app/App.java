a85376ce6c385aa167025c6590bd1c18cb3f087f
ZioCecio <gabricetro@libero.it>
2020-04-20 17:03:58 +0200

    Eskere

--- src/app/App.java
+++ src/app/App.java
@@ -2,7 +2,7 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello Java");
+        System.out.println("HEllo");
         System.err.println("CHE ROTTURA");
         System.out.println("BELLA");
         System.out.println("Mela");
